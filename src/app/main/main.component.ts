cecd6ce4b0d596e8fbd6f4f5111eab7f1b7db818
Marta Komar <m.koomarr@gmail.com>
2019-04-26 08:44:53 +0200

    Zadanie wykonane

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -16,10 +16,10 @@ export class MainComponent implements OnInit {
    this.list = this._service.list;
   }
 
-onSearchValueChanges(inputElement: HTMLInputElement){
+onSearchValueChanges(inputElement: HTMLInputElement) {
 
   if (inputElement.value.length > 0) {
-  alert('Teraz funkcja wywołana keyup powinna sprawdzić, czy to, co zostało wpisane do inputa znajduje się w [planetsList] i jeśli tak, to kasuje stary widok i wyświetla znalezioną wartość (wszystko, co jej dotyczy, czyli singlePlanet), opcjonalnie można dodać warunek, że jeśli wpisywana wartość będzie miała value.length > 3, a jeśli nie ma, to wyświetla komunikat "Nic nie znaleziono", żeby wpisujący wiedział, że wyszukiwarka działa. Niestety nie wiem jak to zrobić, bo nie udało mi się znaleźć informacji, jak dostać się do tej listy, żeby ją przeszukać oraz nie wiem jak istniejącą listę zastąpić w DOM wynikiem wyszukiwania :(')
+  alert('Teraz funkcja wywołana keyup powinna sprawdzić, czy to, co zostało wpisane do inputa znajduje się w [planetsList] i jeśli tak, to kasuje stary widok i wyświetla znalezioną wartość (wszystko, co jej dotyczy, czyli singlePlanet), opcjonalnie można dodać warunek, że jeśli wpisywana wartość będzie miała value.length > 3, a jeśli nie ma, to wyświetla komunikat "Nic nie znaleziono", żeby wpisujący wiedział, że wyszukiwarka działa. Niestety nie wiem jak to zrobić, bo nie udało mi się znaleźć informacji, jak dostać się do tej listy, żeby ją przeszukać oraz nie wiem jak istniejącą listę zastąpić w DOM wynikiem wyszukiwania :(');
   }
 }
 
